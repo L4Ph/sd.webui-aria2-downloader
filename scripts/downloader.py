aa7943b616dd5cb891bdd80ea4281cdcaf9249c7
唐澤 克幸 <katsuyukikarasawa@gmail.com>
2023-03-29 18:33:54 +0900

    fixed it not working.
    Thanks! ddddv#9018

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -1,11 +1,11 @@
-import gradio as gr
 import os
 import subprocess
 
-# from modules import script_callbacks
+import gradio as gr
+from modules import script_callbacks
 
 # Path to aria2c binary file
-aria2c_path = "./aria2c.exe"
+aria2c_path = "extensions/sd.webui-model-downloader/scripts/aria2c.exe"
 
 def download_with_aria2c(url, destination):
     process = subprocess.Popen(
@@ -15,6 +15,7 @@ def download_with_aria2c(url, destination):
     stdout, stderr = process.communicate()
     if process.returncode != 0:
         raise Exception(f"Download failed with error code {process.returncode}:\n{stderr.decode('utf-8')}")
+    print("Download complete.")
     return "Download complete."
 
 def on_ui_tabs():
@@ -27,22 +28,28 @@ def on_ui_tabs():
         filename = gr.Textbox(label="Filename")
         download_button = gr.Button(value="Download")
 
-        def download_model():
-            url = download_url.value
-            filename_value = filename.value or "downloaded_file"
+        def download_model(download_url, dropdown, filename):
+            url = download_url
+            filename = filename or "downloaded_file"
             destination = ""
-            if dropdown.value == "Stable Diffusion":
-                destination = os.path.join("/../../../models/Stable-diffusion", "models", f"{filename_value}.pt")
-            elif dropdown.value == "LoRA":
-                destination = os.path.join("/../../../models/Lora", "models", f"{filename_value}.pt")
-            elif dropdown.value == "VAE":
-                destination = os.path.join("/../../../models/vae", "models", f"{filename_value}.pt")
-            elif dropdown.value == "Embeddings":
-                destination = os.path.join("/../../../embeddings", "models", f"{filename_value}.pt")
+            
+            if dropdown == "Stable Diffusion":
+                destination = "models/Stable-diffusion/models/" + filename
+            elif dropdown == "LoRA":
+                destination = "models/Lora/models/" + filename
+            elif dropdown == "VAE":
+                destination = "models/vae/models/" + filename
+            elif dropdown == "Embeddings":
+                destination = "embeddings/models/" + filename
+                print(destination)
             download_with_aria2c(url, destination)
 
-        download_button.action = download_model
+        download_button.click(
+            fn=download_model,
+            inputs=[download_url,dropdown,filename],
+            outputs=[],
+        )
         return [(ui_component, "Model Downloader", "model_downloader_tab")]
 
 
-# script_callbacks.on_ui_tabs(on_ui_tabs)
+script_callbacks.on_ui_tabs(on_ui_tabs)
